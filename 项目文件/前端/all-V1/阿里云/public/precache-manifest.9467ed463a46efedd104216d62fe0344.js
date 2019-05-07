02ea781359e3d9064a6363f158bdc3faff466d0d
peterchen <peterchenzhy@aliyun.com>
2019-05-07 21:30:20 +0800

    no message

---
+++ 项目文件/前端/all-V1/阿里云/public/precache-manifest.9467ed463a46efedd104216d62fe0344.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "f06ae755d85cbc383ee0",
+    "url": "/static/css/4.4fcaa062.chunk.css"
+  },
+  {
+    "revision": "107cea14d671e9027b27",
+    "url": "/static/css/0.cadc4253.chunk.css"
+  },
+  {
+    "revision": "a0165072f1efbb34f320",
+    "url": "/static/css/main.a0ef15a9.chunk.css"
+  },
+  {
+    "revision": "a0165072f1efbb34f320",
+    "url": "/static/js/main.7d94bbf6.chunk.js"
+  },
+  {
+    "revision": "63b6feeb0e7c5030b640",
+    "url": "/static/js/runtime~main.14afb76c.js"
+  },
+  {
+    "revision": "891d7e880c1ec9f39aef",
+    "url": "/static/js/3.c7442acb.chunk.js"
+  },
+  {
+    "revision": "107cea14d671e9027b27",
+    "url": "/static/js/0.0141853c.chunk.js"
+  },
+  {
+    "revision": "f06ae755d85cbc383ee0",
+    "url": "/static/js/4.f0357a89.chunk.js"
+  },
+  {
+    "revision": "b9549fd018b8ef41be37",
+    "url": "/static/css/5.160dfde0.chunk.css"
+  },
+  {
+    "revision": "b9549fd018b8ef41be37",
+    "url": "/static/js/5.6a8beed5.chunk.js"
+  },
+  {
+    "revision": "6ca4a84e9689c7457e0b",
+    "url": "/static/css/6.acac0c57.chunk.css"
+  },
+  {
+    "revision": "6ca4a84e9689c7457e0b",
+    "url": "/static/js/6.a056e59f.chunk.js"
+  },
+  {
+    "revision": "25bad4f13629b1149ee076cc1ba40139",
+    "url": "/index.html"
+  }
+];
